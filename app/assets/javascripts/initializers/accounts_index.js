20247360c6fb08e4b3488b8f64cdbb6c6177774c
Andreas Gehret <agehret@gmail.com>
2013-02-11 21:18:44 +0100

    Re-enabled notes feature. fixes #21
            deleted:    app/views/notes/new.html.erb

--- app/assets/javascripts/initializers/accounts_index.js
+++ app/assets/javascripts/initializers/accounts_index.js
@@ -1,10 +1,16 @@
 Initializers.AccountsIndex = function($) {
 
-    console.log('Initializer: AccountsIndex');
-    
-    // example
-    $(document).on('click', 'div', function() {
-        console.log("a div at the following position was clicked:");
-        console.log($(this).position());
-    })
+    // open a modal dialog to add a note
+    $(document).on('click', '.add-note-modal', function() {
+       $('#addNote form').attr("action", "/items/" +  $(this).data('item-id') + "/notes");
+       $('#addNote').modal();
+       return false;
+    });
+
+    // open a modal dialog to show a note
+    $(document).on('click', '.show-note-modal', function() {
+       $('#showNote .modal-body').load("/items/" +  $(this).data('item-id') + "/notes/0");
+       $('#showNote').modal();
+       return false;
+    });
 }
--- app/assets/javascripts/initializers/accounts_overview.js
+++ app/assets/javascripts/initializers/accounts_overview.js
@@ -3,6 +3,5 @@ Initializers.AccountsOverview = function($) {
     $(document).on('click', 'tr.overview_category a', function() {
        var identifier = $(this).data('subcategory');
        $("#" + identifier).toggle();
-    });
-    
+    });    
 }
